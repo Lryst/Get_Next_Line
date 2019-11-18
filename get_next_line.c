8cad7e1b3340135312fad5d8d3cdebfbf2176cc7
Lucie RYST <lryst@e3r5p13.42.fr>
2019-11-18 18:08:50 +0100

    okok

--- get_next_line.c
+++ get_next_line.c
@@ -6,31 +6,44 @@
 /*   By: lryst <lryst@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 14:11:35 by lryst             #+#    #+#             */
-/*   Updated: 2019/11/18 14:06:04 by lryst            ###   ########.fr       */
+/*   Updated: 2019/11/18 18:04:57 by lryst            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int ft_error(fd, **buf, **line)
+int	ft_error(fd, **buf, **line)
 {
-    if (fd == -1 || line == NULL)
-        return (-1);
-    if (!*buf)
-    {
-        if (read(fd, *buf, 0) == -1))
-            return (-1);
-    }
-    return (0);
+	if (fd == -1 || line == NULL)
+		return (-1);
+	if (!*buf)
+	{
+		if (!(char*)malloc(sizeof(char*) BUFFER_SIZE + 1))
+		{
+			free(buf);
+			return (-1);
+		}
+	}
+	return (0);
 }
 
 int	get_next_line(int fd, char **line)
 {
-    if (ft_error(fd, &buf, line) < -1)
-        return (-1);
-    read(fd, buf, BUFFER_SIZE)
+	static char buf[BUFFER_SIZE + 1];
+	int ret;
+
+	if (ft_error(fd, &buf, line) < -1)
+		return (-1);
+	ret = read(fd, buf, BUFFER_SIZE);
+	if (ret == -1)
+		return (-1);
+	buf[ret] = '\0';
 }
 
-gerer les erreurs de bases
-lire une ligne 
-retourner l'interger en focntion de ce qui est lu on non
+int main()
+{
+    fd = open("bonjour", O_RDONLY);
+    ft_putnbr(fd);
+    close(fd);
+    return (0);
+}
