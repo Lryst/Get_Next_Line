64c4a08ba274ae9c3c0ab2b070af09406b83b695
Lucie RYST <lryst@e2r3p9.42.fr>
2019-12-04 21:03:01 +0100

    finish

--- main.c
+++ main.c
@@ -6,16 +6,16 @@ int		main()
 	static char *line = NULL;
 	int res;
 
-	fd = open("/Users/lulu/Lryst/Get_Next_Line/42TESTERS-GNL/results/result2.txt", O_RDONLY);
+	fd = open("./bonjour.txt", O_RDONLY);
     while ((res = get_next_line(fd, &line)) > 0)
 	{
-		printf("resultat : |%d| -> %s \n", res, line);
+		printf("resultat : |%d| -> %s\n", res, line);
 		free(line);
 		line = NULL;
 	}
-	printf("resultat : |%d| -> %s \n", res, line);
+	printf("resultat : |%d| -> %s\n", res, line);
 
-   // system("leaks a.out");
+   system("leaks a.out");
 
     return (0);
 }
